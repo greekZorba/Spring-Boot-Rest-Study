e4ed2aa355f7537d23a2ba0041c3e0447164ef9c
jinhak Kim <smiling.jinhak@gmail.com>
2019-05-18 21:23:37 +0900

    HAL 브라우저 기능 추가, 스프링 부트 데이터 레스트 마무리

--- data-rest/src/main/java/com/community/rest/repository/BoardRepository.java
+++ data-rest/src/main/java/com/community/rest/repository/BoardRepository.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 // 기본 path가 boards임. path에 입력해서 원하는대로 경로 설정 가능
 @RepositoryRestResource(excerptProjection = BoardOnlyContainTitle.class, path = "boards")
+// 아래 'exported = false'은 특정 리포지토리, 쿼리 메서드, 필드를 노출하고 싶지 않을 때 사용한다.
+//@RepositoryRestResource(exported = false)
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Override
@@ -19,5 +21,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     <S extends Board> S save(S entity);
 
     @RestResource(path = "query")
+//    @RestResource(exported = false)
     List<Board> findByTitle(@Param("title") String title);
 }
